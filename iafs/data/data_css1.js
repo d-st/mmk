54c61188cd0a09347aab100a93d9cec72cd91ff8
Daniel Steinberger <daniel.steinberger@mni.fh-giessen.de>
2010-12-16 02:54:22 +0100

    Templates

---
+++ iafs/data/data_css1.js
@@ -0,0 +1,9 @@
+﻿var data_css1 = (function () {
+
+return [
+'void(0)', 'CSS-Snippets',
+
+"%3C%21DOCTYPE%20html%3E%0A%3Chtml%20lang%3D%22de%22%3E%0A%3Chead%3E%0A%3Ctitle%3ETitelzeile%3C/title%3E%0A%3Cmeta%20charset%3D%22utf-8%22%20/%3E%0A%3Clink%20rel%3D%22stylesheet%22%20href%3D%22../incl/iafs.css%22%20/%3E%0A%3Cscript%20type%3D%22text/javascript%22%20src%3D%22../incl/debug.js%22%3E%3C/script%3E%0A%3Cscript%20type%3D%22text/javascript%22%20src%3D%22../incl/browser.js%22%3E%3C/script%3E%0A%3Cscript%20type%3D%22text/javascript%22%20src%3D%22../incl/visuell.js%22%3E%3C/script%3E%0A%3Cscript%20type%3D%22text/javascript%22%20src%3D%22../incl/dom.js%22%3E%3C/script%3E%0A%3Cscript%20type%3D%22text/javascript%22%20src%3D%22../incl/crypt.js%22%3E%3C/script%3E%0A%3Cscript%20type%3D%22text/javascript%22%20src%3D%22../incl/local.js%22%3E%3C/script%3E%0A%3Cscript%3E%0A%0Awindow.onload%20%3D%20function%20%28%29%20%7B%20%7D%0A%3C/script%3E%0A%3C/head%3E%0A%3Cbody%20id%3D%22body%22%3E%0A%0A%3Ch1%3E%E4%F6%FC%C4%D6%DC%DF%u2750%u25F9%uA867%u2D3D%u2604%u2624%u2616%u2617%u27F0%u2AF1%u2646%u26B6%u2D56%u2D36%u2EDE%u2ED8%3C/h1%3E%0A%3Cp%3E%20%0A%3Ca%20href%3D%22javascript%3Aalert%28escape%28%27%E4%F6%FC%C4%D6%DC%DF%u2750%u25F9%uA867%u2D3D%u2604%u2624%u2616%u2617%u27F0%u2AF1%u2646%u26B6%u2D56%u2D36%u2EDE%u2ED8%27%29%29%22%3E%20escape%28%29%20%3C/a%3E%0A%0A%3C/p%3E%0A%3Cform%20action%3D%22%22%3E%3Cpre%3E%0A%3Ctextarea%20id%3D%220%22%20rows%3D%2220%22%20cols%3D%2280%22%20title%3D%22%22%3E%3C/textarea%3E%0A%3C/pre%3E%3C/form%3E%0A%0A%3C/body%3E%0A%3C/html%3E",
+"Basic CSS"
+];
+})();
---
+++ iafs/incl/templates.js
@@ -0,0 +1,30 @@
+﻿var templates = (function (win, doc) {
+    // private
+	var template = [],
+	_get_name = function(s) {
+		do {
+			name = prompt(s)
+		} while (name == "");
+		return name;
+	};
+	
+    return { //public
+		new: function() {
+			template = [];
+		},
+		append: function(tmpl) {
+			name = _get_name("Bitte einen Namen für diese Schablone eingeben");
+			template.push([escape(tmpl), name]);
+		},
+		load: function(txta) {
+			name = _get_name("Bitte einen Namen für diese Sammlung eingeben");
+			s = "var data_ = (function () {\n\nreturn [\n'void(0)', '"+name+"'";
+			for(var i=0; i<template.length; i+=1) {
+				s += ',\n\n"'+template[i][0]+'",\n"'+template[i][1]+'"';
+			}
+			s += "\n\n];\n})();\n"
+			return s;
+		},
+        ende: 'ende'
+    };
+})(window, document);
